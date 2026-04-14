2b862ac05b613d3df1b8c28f1adacf2008e14a5c
amemomemo-debug <amemomemo@gmail.com>
2026-04-14 16:57:45 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("نظام شؤون الموظفين")
+root.geometry("800x500")
+
+label = tk.Label(root, text="البرنامج شغال ✔", font=("Arial", 16))
+label.pack(pady=20)
+
+root.mainloop()
